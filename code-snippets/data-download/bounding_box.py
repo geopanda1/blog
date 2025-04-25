628395113df9be3704257d6217424d397f0ffe7d
andreas kollert <andreas.kollert@uibk.ac.at>
2025-04-25 19:56:08 +0200

    stac stuff and xarray

---
+++ code-snippets/data-download/bounding_box.py
@@ -0,0 +1,16 @@
+# bounding box Stubaier/Oetztaler Alps
+# EPSG:32632
+x_min, y_min, x_max, y_max = (
+    621714.2562144251,
+    5170532.115198855,
+    674998.6205839745,
+    5237277.406921703,
+)
+# bounding box Stubaier/Oetztaler Alps
+# EPSG:4326
+lon_min, lat_min, lon_max, lat_max = (
+    10.591419440110746,
+    46.665472557114995,
+    11.313377218439005,
+    47.27767123235298,
+)
