067a618b099ad920dbe604669d83f3f2846c628f
andreas kollert <andreas.kollert@uibk.ac.at>
2025-05-01 18:34:12 +0200

    lwd tirol post

---
+++ posts/lwd-tirol-download/crop.py
@@ -0,0 +1,12 @@
+import skimage
+
+
+path = "LPUI2-snow-depth.png"
+
+pic = skimage.io.imread(path)
+
+ratio = 1200 / 852
+
+cropped = pic[: 426 * 2, : 600 * 2, :]
+
+skimage.io.imsave("sd-crop.png", cropped)
